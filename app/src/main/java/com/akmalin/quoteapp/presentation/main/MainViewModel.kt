d443cc985b7d9606cae6841c79a8fab5812449d6
Akmaliin <akmalnafis25@gmail.com>
2024-04-03 19:50:46 +0700

    Finish Task

--- app/src/main/java/com/akmalin/quoteapp/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/akmalin/quoteapp/presentation/main/MainViewModel.kt
@@ -1,6 +1,25 @@
 package com.akmalin.quoteapp.presentation.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.akmalin.quoteapp.data.model.Quote
 import com.akmalin.quoteapp.data.repository.QuoteRepository
+import com.akmalin.quoteapp.utils.ResultWrapper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel (private val repository:QuoteRepository){
+class MainViewModel(private val repository: QuoteRepository) : ViewModel() {
+    private val _quoteLiveData = MutableLiveData<ResultWrapper<List<Quote>>>()
+    val quoteLiveData: LiveData<ResultWrapper<List<Quote>>>
+        get() = _quoteLiveData
+
+    fun fetchQuotes() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getRandomQuotes().collect {
+                _quoteLiveData.postValue(it)
+            }
+        }
+    }
 }
